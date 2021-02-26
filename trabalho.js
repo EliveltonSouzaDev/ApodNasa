0e246a93d9c03f13c1069d9f17632cdaa295244c
EliveltonSouzaDev <tst.elivelton@gmail.com>
2021-02-26 15:46:24 -0300

    update

--- trabalho.js
+++ trabalho.js
@@ -1,9 +1,11 @@
+const {
+    addDays,
+    subDays,
+    isToday,
+    format
+} = globalThis.dateFns;
 
-const { addDays, subDays, isToday, format } = globalThis.dateFns;
-
-const input = document.getElementById('data');
-
-
+// modelo para os dados serem inseridos
 class NasaModel {
 
     constructor() {
@@ -25,21 +27,19 @@ class NasaModel {
         return this._title;
     }
 
-    get input(){
+    get input() {
         return this._input;
 
     }
 
+    // metodo que busca dados da API
     buscaDados() {
 
         let request = new XMLHttpRequest();
 
-        // passar como parametro a data
+        // passar como parâmetro a data
 
-        let input = document.getElementById('data');
-
-
-        request.open("GET", `https://api.nasa.gov/planetary/apod?api_key=4NIkTZ3CfK1HkJ5vuRxze67FaTVzbJMRdPSznQ2R&date=${input.value}`, false);
+        request.open("GET", `https://api.nasa.gov/planetary/apod?api_key=4NIkTZ3CfK1HkJ5vuRxze67FaTVzbJMRdPSznQ2R&date=${this._input}`, false);
 
         request.addEventListener('load', () => {
 
@@ -51,7 +51,6 @@ class NasaModel {
                 this._text = response.explanation;
                 this._title = response.title;
                 this._input = response.date;
-
 
             } else {
                 console.log("Um código inesperado foi retornado!");
@@ -65,30 +64,27 @@ class NasaModel {
     }
 };
 
+// pega as divs já criadas no html e insere os dados nela.
+
 class NasaViews {
 
     constructor() {
-
         this._text = document.getElementById("p")
         this._img = document.getElementById("img")
         this._title = document.getElementById("title")
-        this._input = document.getElementById("data")
-
-
+        this._input = document.getElementById("date-txt")
     }
 
-    criaElementos(text, img, title,input) {
-
+    criaElementos(text, img, title, input) {
 
         this._img.src = img;
         this._text.textContent = text;
         this._title.textContent = title;
         this._input.innerHTML = input;
-
-
     }
 };
 
+// busca os dados da model junto com o views e insere
 class NasaController {
 
     buscarInfo() {
@@ -96,18 +92,19 @@ class NasaController {
         let dados = new NasaModel()
         dados.buscaDados()
 
-        let desenha = new NasaViews()
+        let desenha = new NasaViews(dados)
         desenha.criaElementos(dados.text, dados.img, dados.title, dados.input);
 
     }
 
-    teste(){
-        var d = new Date();
-
-        console.log(d.getDate());
+    buttonNext() {
+        let data = document.getElementById('date-text')
 
     }
 
+    buttonPrevious() {
+        let data = document.getElementById('date-text')
+    }
 }
 
 let controller = new NasaController();
@@ -119,11 +116,65 @@ btn.addEventListener('click', () => {
 });
 
 
-// botoes
+var hoje = addDays(new Date(), 1);
 
-/*var hoje = addDays(new Date(), 1);
+hoje = format(new Date(), 'YYYY-MM-DD');
 
-hoje = format(new Date(input.value),'YYYY-MM-DD')
+console.log(hoje);
+
+// ao carregar a página aparece a imagem do dia
+window.onload = controller.buscarInfo(hoje);
+
+
+// pega os ids dos botões
+let buttonFind = document.getElementById("arquived");
+let buttonPrevious = document.getElementById("previous");
+let buttonNext = document.getElementById("next");
+
+
+// Busca o dia posterior e remove o dia atual
+buttonNext.addEventListener("click", function () {
+
+
+    controller.buttonNext()
+});
+
+buttonPrevious.addEventListener("click", function () {
+
+    controller.buttonPrevious();
+
+    var data = document.getElementById('data');
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+var hoje = addDays(new Date(), 1);
+
+hoje = format(new Date(),'YYYY-MM-DD')
 
 console.log(hoje);
 
@@ -134,35 +185,3 @@ console.log(result2);
 
 var  ontem = subDays(new Date(), 1,)
 console.log(ontem);*/
-
-
-// ao carregar a página aparece a imagem do dia
-
-window.onload = controller.buscarInfo();
-
-
-let buttonFind = document.getElementById("arquived");
-let buttonPrevious = document.getElementById("previous");
-
-let buttonNext = document.getElementById("next");
-
-
-// Busca o dia posterior e remove o dia atual
-buttonNext.addEventListener("click", function () {
-    
-
-    controller.teste()
-
-});
-
-buttonPrevious.addEventListener("click", function () {
-
-    controller.buscarInfo();
-
-    var data = document.getElementById('data');
-
-
-});
-
-
- 
